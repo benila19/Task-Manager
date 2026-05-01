22c827c0d40dfeb9a0a7bbf5f8405a9f5b815389
benila19 <your-email@example.com>
2026-05-02 04:41:52 +0530

    fix db connection error

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://localhost:5000";
-
+export const BASE_URL = import.meta.env.VITE_API_URL;
 export const API_PATHS = {
     AUTH: {
         SIGNUP: "/api/auth/register",
